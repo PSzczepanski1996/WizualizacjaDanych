4b440f8ba2cd3efc215d487a3db85d7f44f401c8
PSzczepanski1996 <p.szczepanski996@gmail.com>
2020-05-09 09:55:58 +0200

    Finish zad11, rest is zad12

--- 3_wprowadzenie/zad11.py
+++ 3_wprowadzenie/zad11.py
@@ -1,4 +1,5 @@
-from .liczby_zespolone import *
+from liczby_zespolone import operacje, wartosci  # noqa
 
 pierwsza = wartosci.liczba_zespolona(3, 1)
 druga = wartosci.liczba_zespolona(5, 2)
+operacje.licz_zespolone(pierwsza, druga)
